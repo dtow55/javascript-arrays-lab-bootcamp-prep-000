f9019eaa71ce45b10ca0234f39a42d6b77675247
David J Towster <djt4xe@virginia.edu>
2017-11-27 07:11:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function prependKitten(name) {
 
 function removeLastKitten() {
   var newArray = kittens.slice(0,kittens.length - 1);
-  return newArray;
+  return newArray;  
 }
 
 function removeFirstKitten() {
