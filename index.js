ad39364776b6b500670c9480ba74b7cf1073dac8
David J Towster <djt4xe@virginia.edu>
2017-11-27 06:35:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  kittens.pop("Billy")
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  
 }
